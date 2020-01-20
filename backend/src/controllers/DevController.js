37a9caa095773250f833c3d0b7b513e283640d47
iangYo <iangabrielrodrigues@gmail.com>
2020-01-20 18:22:02 -0300

    websocket adicionado na aplicação mobile

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
+
 
 module.exports = {
     async index(req, res) {
@@ -34,6 +36,13 @@ module.exports = {
                 techs: techsArray,
                 location
             });
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            )
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
         return res.json(dev);
